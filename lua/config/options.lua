e021635775369dab0e7b6087faceeeac916d3b4e
bitSheriff <bitSheriff@protonmail.com>
2024-07-29 12:03:16 +0200

    save colorscheme and apply it at startup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -29,3 +29,18 @@ vim.opt.conceallevel = 1
 
 -- set soft wrapping as default
 -- require("wrapping").soft_wrap_mode()
+--
+
+-- save the colorscheme
+vim.api.nvim_create_autocmd("VimEnter", {
+  nested = true,
+  callback = function()
+    pcall(vim.cmd.colorscheme, vim.g.SCHEME)
+  end,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = function(params)
+    vim.g.SCHEME = params.match
+  end,
+})
