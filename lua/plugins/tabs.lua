00039d2d701680a7e7466f8f5c437af8f2ef5c58
bitSheriff <root@bitsheriff.dev>
2024-10-07 14:28:56 +0200

    added plugin for tab jumping

---
+++ lua/plugins/tabs.lua
@@ -0,0 +1,24 @@
+return {
+
+  -- easy swith tabs/buffers
+  {
+    "leath-dub/snipe.nvim",
+    config = function()
+      require("snipe").setup({
+        hints = {
+          dictrionary = "asdfghjkl",
+        },
+      })
+    end,
+    keys = {
+      {
+        "gb",
+        function()
+          require("snipe").open_buffer_menu()
+        end,
+        desc = "Open Snipe buffer menu",
+      },
+    },
+    opts = {},
+  },
+}
