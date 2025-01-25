9f32452792cc902ad1b6ad6f6fdc0315ca7dcd7f
bitSheriff <root@bitsheriff.dev>
2025-01-25 12:45:03 +0100

    added more hugo snippets

--- lua/plugins/snippets/hugo.lua
+++ lua/plugins/snippets/hugo.lua
@@ -24,7 +24,6 @@ local postfix = require("luasnip.extras.postfix").postfix
 local types = require("luasnip.util.types")
 local parse = require("luasnip.util.parser").parse_snippet
 
-
 -- HUGO markdown
 
 ls.add_snippets("markdown", {
@@ -33,4 +32,56 @@ ls.add_snippets("markdown", {
         i(1),
         t({ "-->" }),
     }),
-}
+})
+
+ls.add_snippets("markdown", {
+    s("hugo github repo", {
+        t({ '{{< github repo="NAME/REPO" >}}' }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo alert generic", {
+        t({ '{{< alert icon="fire" cardColor="#e63946" iconColor="#1d3557" textColor="#f1faee" >}}', "" }),
+        i(1),
+        t({ "", "{{< /alert >}}" }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo article post", {
+        t({ '{{< article link="/docs/welcome/" >}}' }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo figure", {
+        t({
+            "{{< figure",
+            '    src="abstract.jpg"',
+            '    alt="Abstract purple artwork"',
+            '    caption="Photo by [Jr Korpa](https://unsplash.com/@jrkorpa) on [Unsplash](https://unsplash.com/)"',
+            ">}}",
+        }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo keyword", {
+        t({ "{{< keyword >}} *Super* skill {{< /keyword >}}" }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo keyword icon", {
+        t({ '{{< keyword icon="github" >}} Lorem ipsum dolor. {{< /keyword >}}' }),
+    }),
+})
+
+ls.add_snippets("markdown", {
+    s("hugo mermaid", {
+        t({ "{{< mermaid >}}", "" }),
+        i(1),
+        t({ "", "{{< /mermaid >}}" }),
+    }),
+})
