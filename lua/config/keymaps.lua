a43e545f05739ddd4bd0ce6c37e354ca5ec26968
bitSheriff <root@bitsheriff.dev>
2025-05-04 16:59:31 +0200

    another keymap change

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -70,8 +70,7 @@ map("n", "<leader>oo", "<cmd> ObsidianQuickSwitch <cr>", { desc = "[O]bsidian Qu
 map("n", "<leader>ot", "<cmd> ObsidianToday <cr>", { desc = "[O]bsidian [T]oday" })
 map("n", "<leader>og", "<cmd> ObsidianFollowLink <cr>", { desc = "[O]bsidian [G]o to Link" })
 map("n", "<leader>os", "<cmd> ObsidianSearch <cr>", { desc = "[O]bsidian [S]earch" })
-map("n", "<leader>o<Enter>", "<cmd> ObsidianToggleCheckbox <cr>", { desc = "[O]bsidian [C]heckbox" })
-map("n", "<leader>op", require("obsidian").util.smart_action(), { desc = "[O]bsidian Smart Action" })
+map("n", "<leader>oc", require("obsidian").util.smart_action(), { desc = "[O]bsidian Smart Action (Checkbox)" })
 
 -- actions
 map("n", "<leader>af", "gg=G", { desc = "[A]ction code [f]ormat while file" })
