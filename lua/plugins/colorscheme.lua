d0fbf0e4948e0708b1603c3e4a75c427526d8b77
bitSheriff <root@bitsheriff.dev>
2025-10-23 16:52:32 +0200

    added wallust theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,52 +1,63 @@
 return {
 
-  -- Actual colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
-
-  { "EdenEast/nightfox.nvim" },
-
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
-
-  { "catppuccin/nvim", name = "catppuccin" },
-
-  {
-    "LunarVim/synthwave84.nvim",
-    config = function()
-      require("synthwave84").setup({
-        glow = {
-          error_msg = true,
-          type2 = true,
-          func = true,
-          keyword = true,
-          operator = true,
-          buffer_current_target = true,
-          buffer_visible_target = true,
-          buffer_inactive_target = true,
+    -- Actual colorscheme
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
         },
-      })
-    end,
-  },
+    },
 
-  { "rebelot/kanagawa.nvim" },
-  { "maxmx03/dracula.nvim" },
+    { "EdenEast/nightfox.nvim" },
 
-  {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "night" },
+    },
 
-  { "savq/melange-nvim" },
+    { "catppuccin/nvim", name = "catppuccin" },
+
+    {
+        "LunarVim/synthwave84.nvim",
+        config = function()
+            require("synthwave84").setup({
+                glow = {
+                    error_msg = true,
+                    type2 = true,
+                    func = true,
+                    keyword = true,
+                    operator = true,
+                    buffer_current_target = true,
+                    buffer_visible_target = true,
+                    buffer_inactive_target = true,
+                },
+            })
+        end,
+    },
+
+    { "rebelot/kanagawa.nvim" },
+    { "maxmx03/dracula.nvim" },
+
+    {
+        "eldritch-theme/eldritch.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+
+    { "savq/melange-nvim" },
+
+    {
+        "RedsXDD/neopywal.nvim",
+        name = "neopywal",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("neopywal").setup({
+                -- use_wallust = true,
+                -- colorscheme_file = os.getenv("HOME") .. "/.cache/wallust/colors_neopywal.vim",
+            })
+        end,
+    },
 }
-
--- theme will be set in the after/plugin/defaults.lua file
